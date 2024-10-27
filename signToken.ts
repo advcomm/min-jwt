cf903b3c12cdb0d7437704a133912f7f8c5b34eb
Harley Davidson <harley.dvdson@gmail.com>
2024-10-28 02:35:43 +0500

    Check existence of env var

--- signToken.ts
+++ signToken.ts
@@ -3,6 +3,7 @@ import { createPrivateKey, createSign, KeyObject, createSecretKey} from 'node:cr
 export const signToken = function(alg: string, payload: JSON): string {
 	const algs = ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512'];
 	if (!algs.includes(alg)) throw new Error('Algorithm not supported');
+    if (!process.env.pemPrivateKey) throw new Error('Public Key not found');
 	try {
         const secret = createPrivateKey(process.env.pemPrivateKey);
     	if (!secret) throw new Error('Could not find private key');
